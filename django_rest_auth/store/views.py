94c633d10daf9f4be224ebe6c0b49a258dcc65c2
ASWINDAS <cvaswin1997@gmail.com>
2023-12-29 02:41:09 +0530

    Add custompagination class for providing custom count and getting specific page

--- django_rest_auth/store/views.py
+++ django_rest_auth/store/views.py
@@ -3,11 +3,13 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from store.models import Book
 from store.serializers import BookSerializer
+from . pagination import CustomPageNumberPagination
 
 # Create your views here.
 
 class Booklist(generics.ListCreateAPIView):
     permission_classes=[IsAuthenticated]
+    pagination_class=CustomPageNumberPagination
     
     queryset = Book.objects.all()
     serializer_class=BookSerializer
