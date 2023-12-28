11856004d015002679c07c10427372fa7d32cc37
ASWINDAS <cvaswin1997@gmail.com>
2023-12-29 02:09:50 +0530

    Add pagination

--- django_rest_auth/django_rest_auth/settings.py
+++ django_rest_auth/django_rest_auth/settings.py
@@ -94,9 +94,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
     
 }
+
+
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
