43c2cdd1f6829f60f62cb5c38fd9834ed5cd74c2
ASWINDAS <cvaswin1997@gmail.com>
2023-12-29 03:24:36 +0530

    Add cache in booklist view function for better performance

--- django_rest_auth/store/views.py
+++ django_rest_auth/store/views.py
@@ -4,15 +4,42 @@ from rest_framework.permissions import IsAuthenticated
 from store.models import Book
 from store.serializers import BookSerializer
 from . pagination import CustomPageNumberPagination
-
+from django.core.cache import cache
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 # Create your views here.
 
-class Booklist(generics.ListCreateAPIView):
-    permission_classes=[IsAuthenticated]
-    pagination_class=CustomPageNumberPagination
+# class Booklist(generics.ListCreateAPIView):
+#     permission_classes=[IsAuthenticated]
+#     pagination_class=CustomPageNumberPagination
     
-    queryset = Book.objects.all()
-    serializer_class=BookSerializer
+#     queryset = Book.objects.all()
+#     serializer_class=BookSerializer
+
+
+@receiver(post_save, sender=Book)
+@receiver(post_delete, sender=Book)
+def invalidate_book_cache(sender, instance, **kwargs):
+    # Invalidate the cache when a Book object is saved or deleted
+    cache_key = 'book_queryset'
+    cache.delete(cache_key)
+class Booklist(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    pagination_class = CustomPageNumberPagination
+
+    def get_queryset(self):
+        cache_key = 'book_queryset'
+        queryset = cache.get(cache_key)
+
+        if queryset is None:
+            queryset = Book.objects.all()
+            cache.set(cache_key, queryset, timeout=300)  # Cache for 5 min
+
+        return queryset
+
+    serializer_class = BookSerializer
+    
+    
     
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes=[IsAuthenticated]
